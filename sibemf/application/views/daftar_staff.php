7d418476015a109c55aebda44e3003fa517fc1bc
fandazky <fandy12@mhs.if.its.ac.id>
2015-03-08 14:38:21 +0700

    add view report absensi all staff

---
+++ sibemf/application/views/daftar_staff.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <!-- kyaa :3 -->
+    <meta charset="utf-8">
+    <title>Absensi BEM</title>
+
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="octicons.css">
+
+    <link rel="icon"href="img/favicon3.ico" />
+    <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="animatescroll.js"></script>
+
+</head>
+
+<body>
+    <div id = "kipi" class = "halaman">
+        <h1> PENGURUS INTI </h1>
+        <div class ="col-md-4"></div>
+            <div id = "pi" class="col-md-4 section">
+                <img src = "bem.png"/>
+                <h3>Pengurus Inti</h3>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th><span></span></th>
+                    </tr>
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
+                </table>
+            </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <footer>
+    <div class="footer">
+     
+        <div class="footer-right">
+            <span class="octicon octicon-code" ></span> with <span class="glyphicon glyphicon-heart"></span> by TIM DEV MPPL 2015
+        </div>
+    
+    </div>
+    </footer>
+</body>
+</html>
