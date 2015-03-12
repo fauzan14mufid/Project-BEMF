67920bfaa4273cf8e77e35c174106f8ea133e305
fandazky <fandy12@mhs.if.its.ac.id>
2015-03-12 22:53:37 +0700

    Edit view report all staff

--- sibemf/application/views/daftar_staff.php
+++ sibemf/application/views/daftar_staff.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <title>Absensi BEM</title>
 
-    <link rel="stylesheet" href="<?php echo asset_url();?>bootstrap.css">
-    <link rel="stylesheet" href="<?php echo asset_url();?>style.css">
-    <link rel="stylesheet" href="<?php echo asset_url();?>octicons.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="octicons.css">
+    <link rel="stylesheet" href="font-awesome.css">
 
-    <!--<link rel="icon"href="<?php echo asset_url();?>img/favicon3.ico" />-->
-    <script type="text/javascript" src="<?php echo asset_url();?>jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="<?php echo asset_url();?>animatescroll.js"></script>
+    <link rel="icon"href="img/favicon3.ico" />
+    <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="animatescroll.js"></script>
 
 </head>
 
@@ -20,19 +21,333 @@
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
                 <img src = "bem.png"/>
-                <h3>Pengurus Inti</h3>
+                <h3> Pengurus Inti</h3>
                 <table>
                     <tr>
                         <th>Nama</th>
                         <th><span></span></th>
                     </tr>
-
-                    <?php
-                        foreach($Staff as $row){
-                            echo "<tr><a href='all_staff/staff?id=".$row->ID_Staff."'></a><td></td><td><a href='all_staff/staff?id=".$row->ID_Staff."'>".$row->Hadir."</td></tr>";
-                        }
-                    ?>
-
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
+                </table>
+            </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <div id = "srd" class = "halaman">
+        <h1> SRD </h1>
+        <div class ="col-md-4"></div>
+            <div id = "pi" class="col-md-4 section">
+                <img src = "bem.png"/>
+                <h3>Student Recources Development</h3>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th><span></span></th>
+                    </tr>
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
+                </table>
+            </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <div id = "inaf" class = "halaman">
+        <h1> IA </h1>
+        <div class ="col-md-4"></div>
+            <div id = "pi" class="col-md-4 section">
+                <img src = "bem.png"/>
+                <h3>Internal Affairs</h3>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th><span></span></th>
+                    </tr>
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
+                </table>
+            </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <div id = "exaf" class = "halaman">
+        <h1> EA </h1>
+        <div class ="col-md-4"></div>
+            <div id = "pi" class="col-md-4 section">
+                <img src = "bem.png"/>
+                <h3>External Affairs</h3>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th><span></span></th>
+                    </tr>
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
+                </table>
+            </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <div id = "rtd" class = "halaman">
+        <h1> RTD </h1>
+        <div class ="col-md-4"></div>
+            <div id = "pi" class="col-md-4 section">
+                <img src = "bem.png"/>
+                <h3>Research and Technology Development</h3>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th><span></span></th>
+                    </tr>
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
+                </table>
+            </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <div id = "osr" class = "halaman">
+        <h1> OSR </h1>
+        <div class ="col-md-4"></div>
+            <div id = "pi" class="col-md-4 section">
+                <img src = "bem.png"/>
+                <h3>Organization Social Responsibility</h3>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th><span></span></th>
+                    </tr>
+                     
+                    <tr>
+                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
+                        <td><a href ='staff.php?id=1106053546'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
+                        <td><a href ='staff.php?id=1106088146'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
+                        <td><a href ='staff.php?id=1106088461'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
+                        <td><a href ='staff.php?id=1106088783'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
+                        <td><a href ='staff.php?id=1106088202'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
+                        <td><a href ='staff.php?id=1106087824'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
+                        <td><a href ='staff.php?id=1106014854'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
+                        <td><a href ='staff.php?id=1106005736'>5</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
+                        <td><a href ='staff.php?id=1106022244'>4</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
+                        <td><a href ='staff.php?id=1106053552'>4</a></td>
+                    </tr>            
                 </table>
             </div>
         <div class ="col-md-4"></div>
@@ -41,7 +356,7 @@
     <div class="footer">
      
         <div class="footer-right">
-            <span class="octicon octicon-code" ></span> with <span class="glyphicon glyphicon-heart"></span> by TIM DEV MPPL 2015
+            <i class="fa-tachometer"></i> with <i class="fa-heart"></i> by TIM DEV MPPL 2015
         </div>
     
     </div>
