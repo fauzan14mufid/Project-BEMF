150d04a013e28398a4b25a2031f9ff3f547170d5
andrejoshua <andrew.n12@mhs.if.its.ac.id>
2015-03-15 01:35:37 +0700

    Update model + controller all_staff & view all staff

--- sibemf/application/controllers/all_staff.php
+++ sibemf/application/controllers/all_staff.php
@@ -14,6 +14,7 @@ class All_Staff extends CI_Controller {
     public function daftar()
     {
         $data['Staff'] = $this->Staff->getAllStaff()->result();
+        $data['Departemen'] = $this->Departemen->getAllDepartemen()->result();
         $this->load->view('daftar_staff',$data);
     }
 
--- sibemf/application/views/daftar_staff.php
+++ sibemf/application/views/daftar_staff.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <title>Absensi BEM</title>
 
-    <link rel="stylesheet" href="bootstrap.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="octicons.css">
-    <link rel="stylesheet" href="font-awesome.css">
+    <link rel="stylesheet" href="<?php echo asset_url();?>bootstrap.css">
+    <link rel="stylesheet" href="<?php echo asset_url();?>style.css">
+    <link rel="stylesheet" href="<?php echo asset_url();?>octicons.css">
+    <link rel="stylesheet" href="<?php echo asset_url();?>font-awesome.css">
 
-    <link rel="icon"href="img/favicon3.ico" />
-    <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="animatescroll.js"></script>
+    <link rel="icon"href="<?php echo asset_url();?>img/favicon3.ico" />
+    <script type="text/javascript" src="<?php echo asset_url();?>jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="<?php echo asset_url();?>animatescroll.js"></script>
 
 </head>
 
@@ -20,7 +20,7 @@
         <h1> PENGURUS INTI </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "bem.png"/>
+                <img src = "<?php echo asset_url();?>bem.png"/>
                 <h3> Pengurus Inti</h3>
                 <table>
                     <tr>
@@ -76,54 +76,24 @@
         <h1> SRD </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "bem.png"/>
-                <h3>Student Recources Development</h3>
+                <img src = "<?php echo asset_url();?>bem.png"/>
+                <h3><?php
+                    foreach($Departemen as $row){
+                        if($row->ID_Departemen == 1) echo $row->Nama;
+                    }
+                    ?></h3>
                 <table>
                     <tr>
                         <th>Nama</th>
                         <th><span></span></th>
                     </tr>
-                     
-                    <tr>
-                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
-                        <td><a href ='staff.php?id=1106053546'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
-                        <td><a href ='staff.php?id=1106088146'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
-                        <td><a href ='staff.php?id=1106088461'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
-                        <td><a href ='staff.php?id=1106088783'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
-                        <td><a href ='staff.php?id=1106088202'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
-                        <td><a href ='staff.php?id=1106087824'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
-                        <td><a href ='staff.php?id=1106014854'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
-                        <td><a href ='staff.php?id=1106005736'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
-                        <td><a href ='staff.php?id=1106022244'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
-                        <td><a href ='staff.php?id=1106053552'>4</a></td>
-                    </tr>            
+                    <?php
+                        foreach($Staff as $row){
+                            if($row->ID_Departemen == 1){
+                                echo "<tr><td><a href='staff/".$row->ID_Staff."'>".$row->Nama."</a></td><td><a href='staff/".$row->ID_Staff."'>".$row->Hadir."</a></td></tr>";
+                            }
+                        }
+                    ?>
                 </table>
             </div>
         <div class ="col-md-4"></div>
@@ -132,54 +102,25 @@
         <h1> IA </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "bem.png"/>
-                <h3>Internal Affairs</h3>
+                <img src = "<?php echo asset_url();?>bem.png"/>
+                <h3><?php
+                    foreach($Departemen as $row){
+                        if($row->ID_Departemen == 2) echo $row->Nama;
+                    }
+                    ?></h3>
                 <table>
                     <tr>
                         <th>Nama</th>
                         <th><span></span></th>
                     </tr>
-                     
-                    <tr>
-                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
-                        <td><a href ='staff.php?id=1106053546'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
-                        <td><a href ='staff.php?id=1106088146'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
-                        <td><a href ='staff.php?id=1106088461'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
-                        <td><a href ='staff.php?id=1106088783'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
-                        <td><a href ='staff.php?id=1106088202'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
-                        <td><a href ='staff.php?id=1106087824'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
-                        <td><a href ='staff.php?id=1106014854'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
-                        <td><a href ='staff.php?id=1106005736'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
-                        <td><a href ='staff.php?id=1106022244'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
-                        <td><a href ='staff.php?id=1106053552'>4</a></td>
-                    </tr>            
+
+                    <?php
+                    foreach($Staff as $row){
+                        if($row->ID_Departemen == 2){
+                            echo "<tr><td><a href='staff/".$row->ID_Staff."'>".$row->Nama."</a></td><td><a href='staff/".$row->ID_Staff."'>".$row->Hadir."</a></td></tr>";
+                        }
+                    }
+                    ?>
                 </table>
             </div>
         <div class ="col-md-4"></div>
@@ -188,54 +129,25 @@
         <h1> EA </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "bem.png"/>
-                <h3>External Affairs</h3>
+                <img src = "<?php echo asset_url();?>bem.png"/>
+                <h3><?php
+                    foreach($Departemen as $row){
+                        if($row->ID_Departemen == 3) echo $row->Nama;
+                    }
+                    ?></h3>
                 <table>
                     <tr>
                         <th>Nama</th>
                         <th><span></span></th>
                     </tr>
-                     
-                    <tr>
-                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
-                        <td><a href ='staff.php?id=1106053546'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
-                        <td><a href ='staff.php?id=1106088146'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
-                        <td><a href ='staff.php?id=1106088461'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
-                        <td><a href ='staff.php?id=1106088783'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
-                        <td><a href ='staff.php?id=1106088202'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
-                        <td><a href ='staff.php?id=1106087824'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
-                        <td><a href ='staff.php?id=1106014854'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
-                        <td><a href ='staff.php?id=1106005736'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
-                        <td><a href ='staff.php?id=1106022244'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
-                        <td><a href ='staff.php?id=1106053552'>4</a></td>
-                    </tr>            
+
+                    <?php
+                    foreach($Staff as $row){
+                        if($row->ID_Departemen == 3){
+                            echo "<tr><td><a href='staff/".$row->ID_Staff."'>".$row->Nama."</a></td><td><a href='staff/".$row->ID_Staff."'>".$row->Hadir."</a></td></tr>";
+                        }
+                    }
+                    ?>
                 </table>
             </div>
         <div class ="col-md-4"></div>
@@ -244,54 +156,25 @@
         <h1> RTD </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "bem.png"/>
-                <h3>Research and Technology Development</h3>
+                <img src = "<?php echo asset_url();?>bem.png"/>
+                <h3><?php
+                    foreach($Departemen as $row){
+                        if($row->ID_Departemen == 4) echo $row->Nama;
+                    }
+                    ?></h3>
                 <table>
                     <tr>
                         <th>Nama</th>
                         <th><span></span></th>
                     </tr>
-                     
-                    <tr>
-                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
-                        <td><a href ='staff.php?id=1106053546'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
-                        <td><a href ='staff.php?id=1106088146'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
-                        <td><a href ='staff.php?id=1106088461'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
-                        <td><a href ='staff.php?id=1106088783'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
-                        <td><a href ='staff.php?id=1106088202'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
-                        <td><a href ='staff.php?id=1106087824'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
-                        <td><a href ='staff.php?id=1106014854'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
-                        <td><a href ='staff.php?id=1106005736'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
-                        <td><a href ='staff.php?id=1106022244'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
-                        <td><a href ='staff.php?id=1106053552'>4</a></td>
-                    </tr>            
+
+                    <?php
+                    foreach($Staff as $row){
+                        if($row->ID_Departemen == 4){
+                            echo "<tr><td><a href='staff/".$row->ID_Staff."'>".$row->Nama."</a></td><td><a href='staff/".$row->ID_Staff."'>".$row->Hadir."</a></td></tr>";
+                        }
+                    }
+                    ?>
                 </table>
             </div>
         <div class ="col-md-4"></div>
@@ -300,56 +183,54 @@
         <h1> OSR </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "bem.png"/>
-                <h3>Organization Social Responsibility</h3>
+                <img src = "<?php echo asset_url();?>bem.png"/>
+                <h3><?php
+                    foreach($Departemen as $row){
+                        if($row->ID_Departemen == 5) echo $row->Nama;
+                    }
+                    ?></h3>
                 <table>
                     <tr>
                         <th>Nama</th>
                         <th><span></span></th>
                     </tr>
-                     
-                    <tr>
-                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
-                        <td><a href ='staff.php?id=1106053546'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
-                        <td><a href ='staff.php?id=1106088146'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
-                        <td><a href ='staff.php?id=1106088461'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
-                        <td><a href ='staff.php?id=1106088783'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
-                        <td><a href ='staff.php?id=1106088202'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
-                        <td><a href ='staff.php?id=1106087824'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
-                        <td><a href ='staff.php?id=1106014854'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
-                        <td><a href ='staff.php?id=1106005736'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
-                        <td><a href ='staff.php?id=1106022244'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
-                        <td><a href ='staff.php?id=1106053552'>4</a></td>
-                    </tr>            
+
+                    <?php
+                    foreach($Staff as $row){
+                        if($row->ID_Departemen == 5){
+                            echo "<tr><td><a href='staff/".$row->ID_Staff."'>".$row->Nama."</a></td><td><a href='staff/".$row->ID_Staff."'>".$row->Hadir."</a></td></tr>";
+                        }
+                    }
+                    ?>
                 </table>
             </div>
+        <div class ="col-md-4"></div>
+    </div>
+    <div id = "osr" class = "halaman">
+        <h1> OSR </h1>
+        <div class ="col-md-4"></div>
+        <div id = "pi" class="col-md-4 section">
+            <img src = "<?php echo asset_url();?>bem.png"/>
+            <h3><?php
+                foreach($Departemen as $row){
+                    if($row->ID_Departemen == 6) echo $row->Nama;
+                }
+                ?></h3>
+            <table>
+                <tr>
+                    <th>Nama</th>
+                    <th><span></span></th>
+                </tr>
+
+                <?php
+                foreach($Staff as $row){
+                    if($row->ID_Departemen == 6){
+                        echo "<tr><td><a href='staff/".$row->ID_Staff."'>".$row->Nama."</a></td><td><a href='staff/".$row->ID_Staff."'>".$row->Hadir."</a></td></tr>";
+                    }
+                }
+                ?>
+            </table>
+        </div>
         <div class ="col-md-4"></div>
     </div>
     <footer>
