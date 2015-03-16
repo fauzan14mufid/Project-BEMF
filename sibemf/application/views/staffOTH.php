8c1b0a925a9e1f9523778796f63bb0352693e6d5
fandazky <fandy12@mhs.if.its.ac.id>
2015-03-16 08:00:37 +0700

    penambahan view staff of the month

---
+++ sibemf/application/views/staffOTH.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo asset_url();?> style_table.css" >
+</head>
+<body>
+    <div class="title">Staff Of The Month</div>
+    <div id="pricing-table" class="clear">
+        <div class="plan">
+            <h3>SRD<span></span></h3>                  
+            <ul>
+                <li><b>10GB</b> Disk Space</li>
+                <li><b>100GB</b> Monthly Bandwidth</li>
+                <li><b>20</b> Email Accounts</li>
+                <li><b>Unlimited</b> subdomains</li>			
+            </ul> 
+        </div>
+        <div class="plan">
+            <h3>IA<span></span></h3>       
+            <ul>
+                <li><b>5GB</b> Disk Space</li>
+                <li><b>50GB</b> Monthly Bandwidth</li>
+                <li><b>10</b> Email Accounts</li>
+                <li><b>Unlimited</b> subdomains</li>			
+            </ul>    
+        </div>
+        <div class="plan">
+            <h3>EA<span></span></h3>
+            <ul>
+                <li><b>3GB</b> Disk Space</li>
+                <li><b>25GB</b> Monthly Bandwidth</li>
+                <li><b>5</b> Email Accounts</li>
+                <li><b>Unlimited</b> subdomains</li>			
+            </ul>
+        </div>	
+    </div>
+    
+    <div id="pricing-table" class="clear">
+        <div class="plan">
+            <h3>RTD<span></span></h3>                  
+            <ul>
+                <li><b>10GB</b> Disk Space</li>
+                <li><b>100GB</b> Monthly Bandwidth</li>
+                <li><b>20</b> Email Accounts</li>
+                <li><b>Unlimited</b> subdomains</li>			
+            </ul> 
+        </div>
+        <div class="plan">
+            <h3>OSR<span></span></h3>       
+            <ul>
+                <li><b>5GB</b> Disk Space</li>
+                <li><b>50GB</b> Monthly Bandwidth</li>
+                <li><b>10</b> Email Accounts</li>
+                <li><b>Unlimited</b> subdomains</li>			
+            </ul>    
+        </div>
+        <div class="plan">
+            <h3>IM<span></span></h3>
+            <ul>
+                <li><b>3GB</b> Disk Space</li>
+                <li><b>25GB</b> Monthly Bandwidth</li>
+                <li><b>5</b> Email Accounts</li>
+                <li><b>Unlimited</b> subdomains</li>			
+            </ul>
+        </div>	
+    </div>
+    
+    
+</body>
+</html>
