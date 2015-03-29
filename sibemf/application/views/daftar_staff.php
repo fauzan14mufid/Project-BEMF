bf82e72daf4b192fd733c3b1f4663f9ab4d0c5e7
fandazky <fandy12@mhs.if.its.ac.id>
2015-03-29 16:58:38 +0700

    update view daftar staff

--- sibemf/application/views/daftar_staff.php
+++ sibemf/application/views/daftar_staff.php
@@ -16,62 +16,6 @@
 </head>
 
 <body>
-    <div id = "kipi" class = "halaman">
-        <h1> PENGURUS INTI </h1>
-        <div class ="col-md-4"></div>
-            <div id = "pi" class="col-md-4 section">
-                <img src = "<?php echo asset_url();?>KABINET.JPG"/>
-                <h3> Pengurus Inti</h3>
-                <table>
-                    <tr>
-                        <th>Nama</th>
-                        <th><span></span></th>
-                    </tr>
-                     
-                    <tr>
-                        <td><a href ='staff.php?id=1106053546'>Abdullah Izzuddiin Alqassam</a></td>
-                        <td><a href ='staff.php?id=1106053546'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088146'>Ananta Besty</a></td>
-                        <td><a href ='staff.php?id=1106088146'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088461'>Andika Dwi Mahendra R</a></td>
-                        <td><a href ='staff.php?id=1106088461'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088783'>Anisa Fathukirani</a></td>
-                        <td><a href ='staff.php?id=1106088783'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106088202'>Annisaa Fitri Shabrina</a></td>
-                        <td><a href ='staff.php?id=1106088202'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106087824'>Fajar Iman</a></td>
-                        <td><a href ='staff.php?id=1106087824'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106014854'>Reza Mauliadi</a></td>
-                        <td><a href ='staff.php?id=1106014854'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106005736'>Siti Jumaliaya</a></td>
-                        <td><a href ='staff.php?id=1106005736'>5</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106022244'>Tyas Meuthia</a></td>
-                        <td><a href ='staff.php?id=1106022244'>4</a></td>
-                    </tr>
-                    <tr>
-                        <td><a href ='staff.php?id=1106053552'>Yahya Eru Cakra</a></td>
-                        <td><a href ='staff.php?id=1106053552'>4</a></td>
-                    </tr>            
-                </table>
-            </div>
-        <div class ="col-md-4"></div>
-    </div>
     <div id = "sospol" class = "halaman">
         <h1> SRD </h1>
         <div class ="col-md-4"></div>
@@ -126,10 +70,10 @@
         <div class ="col-md-4"></div>
     </div>
     <div id = "minatbakat" class = "halaman">
-        <h1> EA </h1>
+        <h1> RTD </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "<?php echo asset_url();?>EA.png"/>
+                <img src = "<?php echo asset_url();?>RTD.png"/>
                 <h3><?php
                     foreach($Departemen as $row){
                         if($row->ID_Departemen == 3) echo $row->Nama;
@@ -153,10 +97,10 @@
         <div class ="col-md-4"></div>
     </div>
     <div id = "relasi" class = "halaman">
-        <h1> RTD </h1>
+        <h1> OSR </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "<?php echo asset_url();?>RTD.png"/>
+                <img src = "<?php echo asset_url();?>OSR.png"/>
                 <h3><?php
                     foreach($Departemen as $row){
                         if($row->ID_Departemen == 4) echo $row->Nama;
@@ -180,10 +124,10 @@
         <div class ="col-md-4"></div>
     </div>
     <div id = "bismit" class = "halaman">
-        <h1> OSR </h1>
+        <h1> EA </h1>
         <div class ="col-md-4"></div>
             <div id = "pi" class="col-md-4 section">
-                <img src = "<?php echo asset_url();?>OSR.png"/>
+                <img src = "<?php echo asset_url();?>EA.png"/>
                 <h3><?php
                     foreach($Departemen as $row){
                         if($row->ID_Departemen == 5) echo $row->Nama;
@@ -207,7 +151,7 @@
         <div class ="col-md-4"></div>
     </div>
     <div id = "kipi" class = "halaman">
-        <h1> OSR </h1>
+        <h1> IM </h1>
         <div class ="col-md-4"></div>
         <div id = "pi" class="col-md-4 section">
             <img src = "<?php echo asset_url();?>IM.png"/>
@@ -237,7 +181,7 @@
     <div class="footer">
      
         <div class="footer-right">
-            <i class="fa-tachometer"></i> with <i class="fa-heart"></i> by TIM DEV MPPL 2015
+            by TIM DEV MPPL 2015
         </div>
     
     </div>
